354537335101bf88d5c4a347974ee76f16c74f1e
Yaroslav <37yaroslav@gmail.com>
2022-09-03 22:04:38 +0300

    Add controlers; add models; add middlewares; add mongodb; add .env

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+require("./db");
 const userRouter = require("./routes/user");
-const PORT = 8080;
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(userRouter);
+app.use(express.json());
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
